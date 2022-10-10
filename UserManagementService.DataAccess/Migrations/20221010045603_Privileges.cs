5a2f8e386514962517a6c5d84abd596d6bb1c8c7
Koval1997 <kovalmax06@gmail.com>
2022-10-10 10:16:03 +0500

    chore: #31ay7d4: bind privileges to roles, add migrations

---
+++ UserManagementService.DataAccess/Migrations/20221010045603_Privileges.cs
@@ -0,0 +1,124 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+using UserManagementService.Core.Entities;
+
+#nullable disable
+
+namespace UserManagementService.DataAccess.Migrations
+{
+    public partial class Privileges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "RoleId",
+                table: "Users",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Privileges",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Privileges", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RolePriveleges",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RoleId = table.Column<long>(type: "bigint", nullable: false),
+                    PrivilegeId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RolePriveleges", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RolePriveleges_Privileges_PrivilegeId",
+                        column: x => x.PrivilegeId,
+                        principalTable: "Privileges",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RolePriveleges_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RolePriveleges_PrivilegeId",
+                table: "RolePriveleges",
+                column: "PrivilegeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RolePriveleges_RoleId",
+                table: "RolePriveleges",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+
+            migrationBuilder.InsertData(
+              table: "Privileges",
+              columns: new[] { "Id", "Name" },
+              values: new object[,]
+              {
+                    { 1L, $"{PrivilegesNames.CanManageUsers}" },
+                    { 2L, $"{PrivilegesNames.CanManageSalary}" },
+                    { 3L, $"{PrivilegesNames.CanManageRoles}" },
+              });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "RolePriveleges");
+
+            migrationBuilder.DropTable(
+                name: "Privileges");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "RoleId",
+                table: "Users",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id");
+        }
+    }
+}
